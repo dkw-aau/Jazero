62cae3957797b6bcd99e80942226ef6b748dd791
MrPekar98 <martin.pekar@outlook.dk>
2025-12-20 11:42:40 +0100

    Show runtime in seconds

--- web/src/main/java/dk/aau/cs/dkwe/edao/jazero/web/view/SearchStatsDialog.java
+++ web/src/main/java/dk/aau/cs/dkwe/edao/jazero/web/view/SearchStatsDialog.java
@@ -6,15 +6,16 @@ import com.vaadin.flow.component.html.H4;
 
 public class SearchStatsDialog extends Dialog
 {
-    public SearchStatsDialog(double reduction, double runtime)
+    public SearchStatsDialog(double reduction, double runtimeNanoseconds)
     {
         setHeaderTitle("Search statistics");
         setWidth("500px");
         setDraggable(true);
         setResizable(true);
 
+        double seconds = runtimeNanoseconds / 1000000000;
         Div content = new Div();
-        String runtimeValue = String.format("%.2f", runtime / 1000), reductionValue = String.format("%.2f", reduction);
+        String runtimeValue = String.format("%.2f", seconds), reductionValue = String.format("%.2f", reduction);
         content.add(new H4("Search runtime: " + runtimeValue + "s"));
         content.add(new H4("Search space reduction: " + reductionValue + "%"));
         add(content);
